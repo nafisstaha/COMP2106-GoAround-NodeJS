132e23eb0d0ff8e95ee0df1a829fe266a6815cb2
Nafiseh Tahaei <nafisehtahaei@Nafisehs-MacBook-Air.local>
2022-03-30 22:46:57 -0400

    Delete and Edit buttons and page

--- controller/landmarks.js
+++ controller/landmarks.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // import Landmark model for CRUD operations
 const Landmark = require('../models/landmark');
 
-/* GET landmarks page. */
+/* GET /landmarks page. */
 router.get('/', (req, res) => {
     // the list of landmarks in mongodb
     Landmark.find((err, landmarks) => {
@@ -20,10 +20,61 @@ router.get('/', (req, res) => {
         }
     })})
 
-/* GET landmarks/create */
+/* GET /landmarks/create */
 router.get('/create', (req, res) => {
     res.render('landmarks/create', {
         title: 'Landmark Details'
+    })
+})
+
+/* POST /landmarks/create => update */
+router.post('/create', (req, res) => {
+    Landmark.create(req.body, (err, landmark) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect('/landmarks')
+        }
+    })
+})
+
+/* GET /landmarks/delete/id => delete landmark with special id */
+router.get('/delete/:_id', (req, res) => {
+    Landmark.remove({ _id: req.params._id }, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect('/landmarks')
+        }
+    })
+})
+
+/* GET /landmarks/edit/id => edit landmark with special id */
+router.get('/edit/:_id', (req, res) => {
+    Landmark.findById(req.params._id, (err, landmark) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.render('landmarks/edit', {
+                title: 'Landmark Details',
+                landmark: landmark
+            })
+        }
+    })
+})
+
+/* POST /landmarks/edit/id => update landmark with special id in mongodb */
+router.post('/edit/:_id', (req, res) => {
+    Landmark.findByIdAndUpdate({ _id: req.params._id }, req.body, null, (err, landmark) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect('/landmarks')
+        }
     })
 })
 
---
+++ public/javascripts/script.js
@@ -0,0 +1,4 @@
+// delete confirmation fory delete button
+function confirmDelete() {
+    return confirm('Are you sure about this deletion?')
+}
