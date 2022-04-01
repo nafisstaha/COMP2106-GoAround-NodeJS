857ce108069ccd175609bda27f34bf9a7b768f59
Nafiseh Tahaei <nafisehtahaei@Nafisehs-MacBook-Air.local>
2022-03-31 21:24:11 -0400

    email validator

--- models/user.js
+++ models/user.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 const plm = require('passport-local-mongoose')
 
+//validator
+const validator = require('validator');
+
 let userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: 'Username is required',
         trim: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error('Invalid Email!')
+            }
+        },
     },
     password: {
         type: String,
